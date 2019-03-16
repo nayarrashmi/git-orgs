cf3bb0e3440dce7c0a29a6dbba7918a05711792f
Rashmi Nair <ranair@paloaltonetworks.com>
2019-03-16 16:35:00 -0700

    adding validation script to help build travis

--- __tests__/components/ReposGrid.test.js
+++ __tests__/components/ReposGrid.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ReposGrid from '../../app/components/ReposGrid';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-test('mounts',()=> {
-  const div = document.createElement('div');
-  ReactDOM.render(<ReposGrid org="netflix"/>, div)
+
+
+configure({ adapter: new Adapter() });
+
+
+test('ReposGrid',()=> {
+  const wrapper = shallow(<ReposGrid org="netflix"/>);
+  expect(wrapper).toMatchSnapshot();
 });
 
